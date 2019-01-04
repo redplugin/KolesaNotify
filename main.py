cf6106f5f0c4403168b9d076d85182ee2d0b7966
redplugin <darmen1@gmail.com>
2019-01-04 23:24:45 +0600

    Initial2

--- main.py
+++ main.py
@@ -12,4 +12,12 @@ getPage.raise_for_status()
 searchResults = bs4.BeautifulSoup(getPage.text, 'html.parser')
 lastUploadedCar = searchResults.select_one('#results .vw-item')
 
-print(lastUploadedCar['id'])
+lastUploadedCarId = lastUploadedCar['id']
+
+conn = smtplib.SMTP('smtp.gmail.com', 587)  # smtp address and port
+conn.ehlo()  # call this to start the connection
+conn.starttls()  # starts tls encryption. When we send our password it will be encrypted.
+conn.login('darmen1@gmail.com', 'Usmc1775le!')
+conn.sendmail('darmen1@gmail.com', 'darmen89@yandex.ru',
+              'Subject: Kolesa notify: ' + lastUploadedCarId)
+conn.quit()
